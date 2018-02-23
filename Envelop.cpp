d8f5786030fe75fae13b88fb496303c383a73e85
snusmumriki <wdart1995@gmail.com>
2018-02-23 13:47:04 +0200

    1

---
+++ Envelop.cpp
@@ -0,0 +1,5 @@
+//
+// Created by MI on 02/23/2018.
+//
+
+#include "Envelop.h"
---
+++ Envelop.h
@@ -0,0 +1,14 @@
+//
+// Created by MI on 02/23/2018.
+//
+
+#ifndef VIKING_ENVELOP_H
+#define VIKING_ENVELOP_H
+
+
+class Envelop {
+
+};
+
+
+#endif //VIKING_ENVELOP_H
